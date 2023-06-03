193fda53404d90eec4824083bf86312b09a15ce0
Luis Vallejo <vallejoaguilar@gmail.com>
2023-06-03 10:55:07 -0500

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noimage from '../assets/no-image-placeholder.webp'
+
 const getCroppedIamgeUrl = (url: string) => {
-  if (!url) return '';
+  if (!url) return noimage;
   const target = 'media/';
   const index = url.indexOf('media/') + 'media/'.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
