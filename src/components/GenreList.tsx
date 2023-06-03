526003855d6684374d88caac9796d783440a9fb2
Luis Vallejo <vallejoaguilar@gmail.com>
2023-06-03 11:56:01 -0500

    Add a dynamic page heading & Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,5 @@
 import useGenres, {Genre} from "../hooks/useGenres";
-import {Button, HStack, Image, List, ListItem, Spinner, Text} from "@chakra-ui/react";
+import {Button, Heading, HStack, Image, List, ListItem, Spinner, Text} from "@chakra-ui/react";
 import getCroppedIamgeUrl from "../services/image-url";
 
 interface Props {
@@ -11,22 +11,35 @@ const GenreList = ({onSelecGenre, selectedGenre}: Props) => {
   const {data, isLoading, error} = useGenres()
 
   if (error) return null;
-
   if (isLoading) return <Spinner/>
 
   return (
-    <List>
-      {data.map(genre => (
-        <ListItem key={genre.id} paddingY='5px'>
-          <HStack>
-            <Image boxSize='32px' borderRadius={8} src={getCroppedIamgeUrl(genre.image_background)}/>
-            <Button fontWeight={genre.id == selectedGenre?.id ? 'bold' : 'normal'}
-                    onClick={() => onSelecGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
-  );
+    <>
+      <Heading fontSize='2xl'> Genres</Heading>
+      <List>
+        {data.map(genre => (
+          <ListItem key={genre.id} paddingY='5px'>
+            <HStack>
+              <Image
+                boxSize='32px'
+                borderRadius={8}
+                src={getCroppedIamgeUrl(genre.image_background)}/>
+              <Button
+                whiteSpace='normal'
+                textAlign='left'
+                fontWeight={genre.id == selectedGenre?.id ? 'bold' : 'normal'}
+                onClick={() => onSelecGenre(genre)}
+                fontSize='lg'
+                variant='link'>{genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
+
+  )
+    ;
 }
 
 export default GenreList;
